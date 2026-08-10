3ead9b496f69642afb4d6ae1214f994a46e92a3e
Helal366 <helal56876@gmail.com>
2026-08-10 19:14:22 +0600

    chore: update business logics in config index file.

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -1,19 +1,50 @@
 import dotenv from "dotenv";
-import path from "path";
 
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
-
-
-export default {
-    node_env: process.env.NODE_ENV,
-    port: process.env.PORT,
-    database_url: process.env.DATABASE_URL,
-    bak_url: process.env.APP_URL,
-    frontend_url: process.env.FRONTEND_URL,
-    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-    jwt_access_secret: process.env.JWT_ACCESS_SECRET!,
-    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET!,
-    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN!,
-    jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN!
+dotenv.config();
+type INodeEnv = "development" | "production";
+interface EnvVariables {
+  NODE_ENV: INodeEnv;
+  PORT: string;
+  DATABASE_URL: string;
+  APP_URL: string;
+  FRONTEND_URL: string;
+  BCRYPT_SALT_ROUNDS: string;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_ACCESS_EXPIRES_IN: string;
+  JWT_REFRESH_EXPIRES_IN: string;
 }
+
+const loadEnvVariables=():EnvVariables=>{
+	const envVars: string[] = [
+    "NODE_ENV",
+    "PORT",
+    "DATABASE_URL",
+    "APP_URL",
+    "FRONTEND_URL",
+    "BCRYPT_SALT_ROUNDS",
+    "JWT_ACCESS_SECRET",
+    "JWT_REFRESH_SECRET",
+    "JWT_ACCESS_EXPIRES_IN",
+    "JWT_REFRESH_EXPIRES_IN",
+  ];
+  envVars.forEach((element)=>{
+	if(!process.env[element]){
+		throw new Error(`Required environmental variable missing: ${element}`);
+	}
+  });
+  return {
+    NODE_ENV: process.env.NODE_ENV as INodeEnv,
+    PORT: process.env.PORT as string,
+    DATABASE_URL: process.env.DATABASE_URL as string,
+    APP_URL: process.env.APP_URL as string,
+    FRONTEND_URL: process.env.FRONTEND_URL as string,
+    BCRYPT_SALT_ROUNDS: process.env.BCRYPT_SALT_ROUNDS as string,
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_ACCESS_EXPIRES_IN: process.env.JWT_ACCESS_EXPIRES_IN as string,
+    JWT_REFRESH_EXPIRES_IN: process.env.JWT_REFRESH_EXPIRES_IN as string,
+  };
+}
+
+export const envVars= loadEnvVariables();
