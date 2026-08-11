d7d9a038dd241e9dc614e56c7de05176d6e020ad
Helal366 <helal56876@gmail.com>
2026-08-12 01:32:56 +0600

    feat: add seed super admin.

---
+++ src/app/utils/seed.ts
@@ -0,0 +1,34 @@
+import bcrypt from "bcryptjs";
+import { Role } from "../../generated/prisma/enums";
+import { envVars } from "../config";
+import { prisma } from "../lib/prisma";
+
+export const seedSuperAdmin = async () => {
+ try {
+     const isSuperAdminExists = await prisma.user.findFirst({
+       where: {
+         role: Role.SUPER_ADMIN,
+       },
+     });
+     if (isSuperAdminExists) {
+       console.log("Super admin exists.");
+       return;
+     };
+     const hashedPassword = await bcrypt.hash(envVars.SUPER_ADMIN_PASSWORD, envVars.BCRYPT_SALT_ROUNDS)
+     const superAdmin = await prisma.user.create({
+       data: {
+         name: envVars.SUPER_ADMIN_NAME,
+         email: envVars.SUPER_ADMIN_EMAIL,
+         password: hashedPassword,
+       },
+     });
+     console.log("Super admin: ", superAdmin)
+ } catch (error) {
+    console.log("Error seeding super admin: ", error);
+    await prisma.user.delete({
+        where:{
+            email: envVars.SUPER_ADMIN_EMAIL
+        }
+    })
+ }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 import { envVars } from "./app/config";
 import { prisma } from "./app/lib/prisma";
+import { seedSuperAdmin } from "./app/utils/seed";
 
 const PORT = envVars.PORT;
 
@@ -8,6 +9,7 @@ const main = async () => {
 	try {
 		await prisma.$connect();
 		console.log("Connected to the database successfully.");
+		await seedSuperAdmin();
 		app.listen(PORT, () => {
 			console.log(`Server is running on port ${PORT}`);
 		});
